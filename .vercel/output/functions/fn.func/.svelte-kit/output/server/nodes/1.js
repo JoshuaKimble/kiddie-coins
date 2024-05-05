30e781b2448b5bd4c761e53ec30ecc1c6e2cf7e7
Joshua <joshuakimble@overstock.com>
2024-05-04 22:54:39 -0600

    Remove .vercel

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.AeMdRPXz.js","_app/immutable/chunks/scheduler.BvLojk_z.js","_app/immutable/chunks/index.D6A44wRX.js","_app/immutable/chunks/entry.BBBD6JX7.js"];
-export const stylesheets = [];
-export const fonts = [];
